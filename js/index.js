8d2f3b5640449d18533b0c9659ee54f1fb55cded
James.C.Wang <clark7704@hotmail.com>
2016-12-27 18:44:33 +0800

    add nav slideup when clikcing

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 /**
  * Created by rimi on 2016/12/23.
  */
+//加载layui导航模块
 layui.use('element', function(){
     var element = layui.element();
-
-
     element.on('tab(nav)', function(data){
         console.log(data);
     });
+});
+//
+$(".layui-nav-item a").click(function () {      //xs页面时点击导航内容自动回收
+    if ($(window).width() < 751) {
+        $('.bs-js-navbar-scrollspy').collapse('hide')
+    }
 });
